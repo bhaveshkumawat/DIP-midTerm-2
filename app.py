69a9ea73f3fa392c462e0cbd2c04135a6eb583e5
bhaveshkumawat <69254383+bhaveshkumawat@users.noreply.github.com>
2021-07-05 12:51:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 #import tensorflow as tf
-from keras.preprocessing import image
+#from keras.preprocessing import image
 import os
 from werkzeug.utils import secure_filename
 st.set_option('deprecation.showfileUploaderEncoding', False)
-from keras.models import load_model
+#from keras.models import load_model
 
 html_temp = """
    <div class="" style="background-color:blue;" >
